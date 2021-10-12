798792cbf37b1d6d577ab937f5ddb217fd963173
Yassaha Ali <yassahaali@gmail.com>
2021-10-12 14:44:24 +0100

    section8 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
-require "DockingStation.rb"
+require "./lib/DockingStation.rb" 
+
+docking_station = DockingStation.new
 
 describe DockingStation do
-
+  #it 'release_bike' do expect(subject).to
+  #respond_to :release_bike
+  it 'DockingStation release bike' do
+    expect(docking_station).to respond_to :release_bike
+  end
 end
