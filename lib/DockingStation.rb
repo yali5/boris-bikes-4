8b25011f0836bbd4d21df3e2da36ab5435ad4ae4
Jenaro Garcia <jjegarcia@hotmail.com>
2021-10-12 17:31:40 +0100

    9.build bike completed

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,4 +1,6 @@
 class DockingStation
   def release_bike
+    bike = Bike.new
   end
 end
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,8 @@ describe DockingStation do
   it 'DockingStation release bike' do
     expect(docking_station).to respond_to :release_bike
   end
+   it 'release bike should return a bike object' do
+    expect(subject.release_bike).to be_instance_of Bike
+  end
+
 end
